97151087f425ffa416ec815b5abe8a37f6a893dc
larry <lcai17@jh.edu>
2024-09-30 13:04:56 -0400

    Add survey title input field and save survey to Firebase

--- client/src/components/SurveyView.js
+++ client/src/components/SurveyView.js
@@ -2,48 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
-
+import Question from './Question';
 const SurveyView = () => {
-  const [surveys, setSurveys] = useState([]);
+    const [surveys, setSurveys] = useState([]);
 
-  useEffect(() => {
-    const fetchSurveys = async () => {
-      const querySnapshot = await getDocs(collection(db, "surveys"));
-      const surveysData = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }));
-      setSurveys(surveysData);
-    };
+    useEffect(() => {
+        const fetchSurveys = async () => {
+            const querySnapshot = await getDocs(collection(db, "surveys"));
+            const surveysData = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setSurveys(surveysData);
+        };
 
-    fetchSurveys();
-  }, []);
+        fetchSurveys();
+    }, []);
 
-  return (
-    <div className="container mt-4">
-      <h2>Your surveys</h2>
-      <br />
-      <ListGroup className="mt-3">
-        {surveys.map((survey, surveyIdx) => (
-          <ListGroup.Item key={survey.id} className="survey">
-            <h3 key={'svy' + surveyIdx}>{survey.title}</h3>
-            <ol>
-              {survey.questions.map((question, questionIdx) => (
-                <>
-                  <li key={'qtn' + questionIdx}>{question.type} - {question.text}</li>
-                  <ol>
-                    {question.options.map((option, optionIdx) => (
-                      <li key={'opt' + optionIdx}>{option}</li>
-                    ))}
-                  </ol>
-                </>
-              ))}
-            </ol>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </div>
-  );
+    return (
+        <div className="container mt-4">
+            <h2>Your surveys</h2>
+            <br />
+            <ListGroup className="mt-3">
+
+                {surveys.map(survey => (
+                    <ListGroup.Item key={survey.id}>
+                        <h3>{survey.title}</h3>
+                        {survey.questions.map((question, index) => (
+                            <Question key={index} question={question} />
+                        ))}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+            <br />
+        </div>
+    );
 };
 
 export default SurveyView;

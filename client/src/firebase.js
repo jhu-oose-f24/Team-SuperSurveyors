ac56ceab7e03196d65863239fc160b76cf410243
Noah Park <noahpark101@gmail.com>
2024-10-04 15:49:55 -0400

    Updated Firebase to main account, updated UML diagram

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,28 +2,29 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const testFirebaseConfig = {
-  apiKey: "AIzaSyB__2yB4LzSSY9ZLJn379hAEfa2CLjZTXg",
-  authDomain: "testsupersurvey.firebaseapp.com",
-  projectId: "testsupersurvey",
-  storageBucket: "testsupersurvey.appspot.com",
-  messagingSenderId: "740469714815",
-  appId: "1:740469714815:web:7d25504f471713d2fd344a",
-  measurementId: "G-YWGV3NQF7M"
-};
-
-// TODO: Config for SuperSurvey app
-// const firebaseConfig = {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "",
-//   measurementId: ""
+// Config for testsupersurvey.firebaseapp.com
+// const testFirebaseConfig = {
+//   apiKey: "AIzaSyB__2yB4LzSSY9ZLJn379hAEfa2CLjZTXg",
+//   authDomain: "testsupersurvey.firebaseapp.com",
+//   projectId: "testsupersurvey",
+//   storageBucket: "testsupersurvey.appspot.com",
+//   messagingSenderId: "740469714815",
+//   appId: "1:740469714815:web:7d25504f471713d2fd344a",
+//   measurementId: "G-YWGV3NQF7M"
 // };
 
-const app = initializeApp(testFirebaseConfig);
+// Config for supersurveyors.firebaseapp.com
+const firebaseConfig = {
+  apiKey: "AIzaSyArgB_tnOWxYUJmYWmDZJOZmfUw3u-qUI8",
+  authDomain: "supersurveryors.firebaseapp.com",
+  projectId: "supersurveryors",
+  storageBucket: "supersurveryors.appspot.com",
+  messagingSenderId: "95534794175",
+  appId: "1:95534794175:web:93e6f03cb8dff33e594d0a",
+  measurementId: "G-C4YGD03GF6"
+};
+
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };

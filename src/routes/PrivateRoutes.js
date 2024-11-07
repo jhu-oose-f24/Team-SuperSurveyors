a9aeca5a2aa9da75b2b92fbba40540ea390803a6
larry <lcai17@jh.edu>
2024-11-07 15:37:25 -0500

    Add react-loader-spinner and replace loading text with audio spinner

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -3,12 +3,13 @@ import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Navigate, Outlet } from 'react-router-dom'
 import { getAuth } from 'firebase/auth'
+import { Audio } from 'react-loader-spinner'
 
 const PrivateRoute = ({ children }) => {
     const [user, loading, error] = useAuthState(getAuth())
 
     if (loading) {
-        return <div>Loading...</div>
+        return <Audio type="Audio" color="#00BFFF" height={80} width={80} />
     }
 
     if (error) {
